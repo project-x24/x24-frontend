bfc48c007cb722d7c7e9e720352b49501636b935
Mohit Khairnar <mvk1407@gmail.com>
2024-03-01 20:19:56 +0530

    feat: added FE codebase with dockerization and organized folde structure

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './styles/index.css';
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <>
+    Hello ! I am Project X-24
+    </>
+  </React.StrictMode>
+);
+
+
